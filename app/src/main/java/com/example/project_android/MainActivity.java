3119282564ee8d7d184e2006ee749234b2c67b05
OriGuez <orig200@gmail.com>
2024-05-30 18:24:51 +0300

    Login

--- app/src/main/java/com/example/project_android/MainActivity.java
+++ app/src/main/java/com/example/project_android/MainActivity.java
@@ -1,24 +1,46 @@
 package com.example.project_android;
-
+import android.app.Activity;
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.textfield.TextInputLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        final EditText passwordEditText = findViewById(R.id.editTextPassword);
+        final TextInputLayout passwordLayout = findViewById(R.id.textInputLayoutPassword);
+
+        passwordEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String password = s.toString();
+                if (password.length() < 8) {
+                    passwordLayout.setError("Password must be at least 8 characters long");
+                } else if (password.matches("\\d+")) {
+                    passwordLayout.setError("Password must contain at least one non-numeric character");
+                } else {
+                    passwordLayout.setError(null);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
         });
     }
 }
